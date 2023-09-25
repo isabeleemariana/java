d028dc5bbdde58a5bce1e9a8729837af8e48cf69
isabeleemariana <126693933+isabeleemariana@users.noreply.github.com>
2023-09-25 09:09:25 -0300

    25/09

--- media.js
+++ media.js
@@ -9,5 +9,16 @@ const nome = form.inNome.value           //obtém o nome
 const nota1 = Number(form.inNota1.value) //obtém a primeira nota
 const nota2 = Number(form.inNota2.value) //obtem a segunda nota
 const media = (nota1 + nora2)/2        //média das notas    
-resp1.innerText = `Média das Notas` ${media.toFixed(2)}
-} 
+resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+
+if(media >= 7) {
+    resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
+    resp2.style.color = "blue"
+} else if(media >=4) {
+    resp2.innerText = `Atenção ${nome}! Você está de exame`
+    resp2.style.color = "green"
+} else {
+    resp2.innerText = `Ops ${nome}! Você foi reprovado(a)`
+    resp2.style.color = "red" 
+}
+})
